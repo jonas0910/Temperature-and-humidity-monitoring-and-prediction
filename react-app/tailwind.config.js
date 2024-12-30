f6d7c90dcce7af991304f8243d2ff7fe04b209fb
jonas <josuecondori2002@gmail.com>
2024-12-30 12:34:20 -0500

    initial configuration and data visualization test

---
+++ react-app/tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,js,jsx,ts,tsx}", // Asegúrate de que Tailwind se aplique a tus archivos JSX/TSX
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
